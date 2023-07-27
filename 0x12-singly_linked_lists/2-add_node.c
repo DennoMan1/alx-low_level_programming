633b0c130f2b9222a195041865b7052c60c589a7
DennoMan1 <denisenderi022@gmail.com>
2023-07-27 10:14:17 -0700

    a function that frees a list_t list.

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,9 +3,9 @@
 #include "lists.h"
 
 /**
- * add_node – add a new node at the beginning of a linked list
+ * add_node - adds a new node at the beginning of a linked list
  * @head: double pointer to the list_t list
- * @str: new string to add to in the node
+ * @str: new string to add in the node
  *
  * Return: the address of the new element, or NULL if it fails
  */
