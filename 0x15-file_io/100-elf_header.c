d648942aa7e59a8877ab518433096dbe5199c847
DennoMan1 <denisenderi022@gmail.com>
2023-08-07 19:47:45 -0700

    a program that displays the information contained in the ELF header at the start of an ELF file.

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,25 +1,26 @@
+100-elf_header.c
+
 #include <stdio.h>
 #include <elf.h>
 #include <sys/types.h>
-#include <stdlib.h>
 #include <sys/stat.h>
-#include <unistd.h>
 #include <fcntl.h>
+#include <stdlib.h>
 
-void print_osabi(unsigned char *e_ident);
-void close_elf(int elf);
 void check_elf(unsigned char *e_ident);
 void print_magic(unsigned char *e_ident);
-void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void print_class(unsigned char *e_ident);
 void print_data(unsigned char *e_ident);
 void print_version(unsigned char *e_ident);
 void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
 void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
 
 /**
  * check_elf - Checks if a file is an ELF file.
- * @e_ident: A pointer to an array containing the ELF magic numbers.
+ * @e_ident: A pointer to an array containing the ELF magic_numbers.
  *
  * Description: If the file is not an ELF file - exit code 98.
  */
@@ -42,7 +43,7 @@ void check_elf(unsigned char *e_ident)
 
 /**
  * print_magic - Prints the magic numbers of an ELF header.
- * @e_ident: A pointer to an array containing the ELF magic numbers.
+ * @e_ident: A pointer to an array containing the ELF magic_numbers.
  *
  * Description: Magic numbers are separated by spaces.
  */
